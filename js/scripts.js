110007771b6e5927fca553c41cd49b9f80dcdfb7
SlavikDemian <slavikdemian@gmail.com>
2023-04-26 18:12:26 +0300

    DZ mobile menu 1

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         margin:20,
         nav:true,
         autoplay: true,
-        autoplayTimeout: 1000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         navText: [ '', '' ],
 
@@ -16,8 +16,20 @@ $(document).ready(function(){
                 items:3
             }
         }
-
-
     });
-
 });
+
+
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
